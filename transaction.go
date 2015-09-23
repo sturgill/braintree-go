c6498b84607bb7a1df15204b144957ef5236529b
Chris Sturgill <chris@thesturgills.com>
2015-09-23 11:34:52 -0500

    Add some missing fields

--- transaction.go
+++ transaction.go
@@ -5,38 +5,53 @@ import (
 	"time"
 )
 
+type TransactionStatusHistory struct {
+	Timestamp *time.Time `xml:"timestamp,omitempty"`
+	Status    string     `xml:"status"`
+	Amount    *Decimal   `xml:"amount"`
+	User      string     `xml:"user,omitempty"`
+	Source    string     `xml:"transaction-source,omitempty"`
+}
+
+type SubscriptionBillingPeriod struct {
+	StartsAt string `xml:"billing-period-start-date,omitempty"`
+	EndsAt   string `xml:"billing-period-end-date,omitempty"`
+}
+
 type Transaction struct {
-	XMLName               string               `xml:"transaction"`
-	Id                    string               `xml:"id,omitempty"`
-	CustomerID            string               `xml:"customer-id,omitempty"`
-	Status                string               `xml:"status,omitempty"`
-	Type                  string               `xml:"type,omitempty"`
-	Amount                *Decimal             `xml:"amount"`
-	OrderId               string               `xml:"order-id,omitempty"`
-	PaymentMethodToken    string               `xml:"payment-method-token,omitempty"`
-	PaymentMethodNonce    string               `xml:"payment-method-nonce,omitempty"`
-	MerchantAccountId     string               `xml:"merchant-account-id,omitempty"`
-	PlanId                string               `xml:"plan-id,omitempty"`
-	CreditCard            *CreditCard          `xml:"credit-card,omitempty"`
-	Customer              *Customer            `xml:"customer,omitempty"`
-	BillingAddress        *Address             `xml:"billing,omitempty"`
-	ShippingAddress       *Address             `xml:"shipping,omitempty"`
-	Options               *TransactionOptions  `xml:"options,omitempty"`
-	ServiceFeeAmount      *Decimal             `xml:"service-fee-amount,attr,omitempty"`
-	CreatedAt             *time.Time           `xml:"created-at,omitempty"`
-	UpdatedAt             *time.Time           `xml:"updated-at,omitempty"`
-	DisbursementDetails   *DisbursementDetails `xml:"disbursement-details,omitempty"`
-	RefundIds             *[]string            `xml:"refund-ids>item,omitempty"`
-	RefundedTransactionId *string              `xml:"refunded-transaction-id,omitempty"`
+	XMLName               string                     `xml:"transaction"`
+	Id                    string                     `xml:"id,omitempty"`
+	CustomerID            string                     `xml:"customer-id,omitempty"`
+	Status                string                     `xml:"status,omitempty"`
+	Type                  string                     `xml:"type,omitempty"`
+	Amount                *Decimal                   `xml:"amount"`
+	OrderId               string                     `xml:"order-id,omitempty"`
+	PaymentMethodToken    string                     `xml:"payment-method-token,omitempty"`
+	PaymentMethodNonce    string                     `xml:"payment-method-nonce,omitempty"`
+	MerchantAccountId     string                     `xml:"merchant-account-id,omitempty"`
+	PlanId                string                     `xml:"plan-id,omitempty"`
+	CreditCard            *CreditCard                `xml:"credit-card,omitempty"`
+	Customer              *Customer                  `xml:"customer,omitempty"`
+	BillingAddress        *Address                   `xml:"billing,omitempty"`
+	ShippingAddress       *Address                   `xml:"shipping,omitempty"`
+	Options               *TransactionOptions        `xml:"options,omitempty"`
+	ServiceFeeAmount      *Decimal                   `xml:"service-fee-amount,attr,omitempty"`
+	CreatedAt             *time.Time                 `xml:"created-at,omitempty"`
+	UpdatedAt             *time.Time                 `xml:"updated-at,omitempty"`
+	DisbursementDetails   *DisbursementDetails       `xml:"disbursement-details,omitempty"`
+	RefundIds             *[]string                  `xml:"refund-ids>item,omitempty"`
+	RefundedTransactionId *string                    `xml:"refunded-transaction-id,omitempty"`
+	StatusHistory         []TransactionStatusHistory `xml:"status-history>status-event,omitempty"`
+	SubscriptionID        string                     `xml:"subscription-id,omitempty"`
+	BillingPeriod         SubscriptionBillingPeriod  `xml:"subscription,omitempty"`
+	AddOnList             []AddOn                    `xml:"add-ons>add-on,omitempty"`
+	DiscountList          []Discount                 `xml:"discounts>discount,omitempty"`
 }
 
 // TODO: not all transaction fields are implemented yet, here are the missing fields (add on demand)
 //
 // <transaction>
 //   <currency-iso-code>USD</currency-iso-code>
-//   <refund-id nil="true"></refund-id>
-//   <refund-ids type="array"/>
-//   <refunded-transaction-id nil="true"></refunded-transaction-id>
 //   <settlement-batch-id>2013-10-08_49grybq7pbtsnvsr</settlement-batch-id>
 //   <custom-fields>
 //   </custom-fields>
@@ -52,35 +67,6 @@ type Transaction struct {
 //   <purchase-order-number nil="true"></purchase-order-number>
 //   <tax-amount nil="true"></tax-amount>
 //   <tax-exempt type="boolean">false</tax-exempt>
-//   <status-history type="array">
-//     <status-event>
-//       <timestamp type="datetime">2013-10-07T17:26:14Z</timestamp>
-//       <status>authorized</status>
-//       <amount>7.00</amount>
-//       <user>eaigner</user>
-//       <transaction-source>Recurring</transaction-source>
-//     </status-event>
-//     <status-event>
-//       <timestamp type="datetime">2013-10-07T17:26:14Z</timestamp>
-//       <status>submitted_for_settlement</status>
-//       <amount>7.00</amount>
-//       <user>eaigner</user>
-//       <transaction-source>Recurring</transaction-source>
-//     </status-event>
-//     <status-event>
-//       <timestamp type="datetime">2013-10-08T07:06:38Z</timestamp>
-//       <status>settled</status>
-//       <amount>7.00</amount>
-//       <user nil="true"></user>
-//       <transaction-source></transaction-source>
-//     </status-event>
-//   </status-history>
-//   <plan-id>bronze</plan-id>
-//   <subscription-id>jqsydb</subscription-id>
-//   <subscription>
-//     <billing-period-end-date type="date">2013-11-06</billing-period-end-date>
-//     <billing-period-start-date type="date">2013-10-07</billing-period-start-date>
-//   </subscription>
 //   <add-ons type="array"/>
 //   <discounts type="array"/>
 //   <descriptor>
